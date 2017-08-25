2c2e74168891a6474117bfe8fe16fe4a48859c98
Sujanth <sujanth1987@gmail.com>
2017-08-25 22:12:55 +1000

    Standard, satellite and hybrid map type added

--- MapViewApplication/ViewController.swift
+++ MapViewApplication/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func standardBtnPressed(_ sender: Any) {
+        mapView.mapType = MKMapType.standard
     }
-
-
+    
+    @IBAction func satalliteBtnPressed(_ sender: Any) {
+        mapView.mapType = MKMapType.satellite
+    }
+    
+    @IBAction func hybridBtnPressed(_ sender: Any) {
+        mapView.mapType = MKMapType.hybrid
+    }
+    
+    @IBAction func locateMeBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func directionBtnPressed(_ sender: Any) {
+    }
 }
 
